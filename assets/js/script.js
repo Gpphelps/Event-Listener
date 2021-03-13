f2089b597b8269b0f187abc5fe7857b5984ff475
mcraet <73713665+mcraeteisha@users.noreply.github.com>
2021-03-13 14:03:42 -0500

    added materialize links to html, updated css, and js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var options = document.querySelector('option');
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+  });
