f9efbecce90730b8471f0539e93083828b93e873
Gene Phelps <gpphelpsii@gmail.com>
2021-03-17 19:39:31 -0400

    added searchable leaflet.js map to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,12 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 var map = L.map('map').fitWorld();
-
+var options = {
+    key: 'c5540aeadb104afa8d407476d1928a32',
+    limit: 10,
+    expand: 'click',
+};
+var control = L.Control.openCageSearch(options).addTo(map);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
